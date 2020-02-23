c8e4c8df42e2d25d7e7c63121f113b0b83fea863
nagi49000 <nagi49000@gmail.com>
2020-02-23 12:23:10 +0000

    first cut of pipeline

---
+++ sklearn_pipeline_play/pipeline.py
@@ -0,0 +1,56 @@
+import pandas as pd
+from argparse import ArgumentParser
+import yaml
+import sys
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+
+class PipelineWrapper:
+    def __init__(self, yaml_dict):
+        self._params = self._get_params_from_args(yaml_dict)
+
+    def _get_params_from_args(self, args):
+        params = {}
+        for step in ['normalise', 'dim_reduce', 'cluster']:
+            params.update({step+'__'+k:v for k,v in args[step].items()})
+        return params
+
+    def _get_pipeline(self, params_dict):
+        p = Pipeline(steps=[('normalise', StandardScaler()),
+                            ('dim_reduce', PCA()),
+                            ('cluster', KMeans())])
+        p.set_params(params_dict)
+        return p
+
+    def fit_transform(self):
+        p = self._get_pipeline(self._params)
+        return p.fit_transform()
+
+class DataIngest:
+    def __init__(self, source):
+        self._source = source
+
+    def get(self):
+        return pd.read_csv(self._source)
+
+class MainWrapper:
+    def __init__(self, argv):
+        self._argv = argv
+
+    def _parse_args(self, cmd_line_list):
+        parser = ArgumentParser()
+        parser.add_argument('--yaml', help='yaml file specifying config to run')
+        args = parser.parse_args(cmd_line_list)
+        return vars(args)
+
+    def run(self):
+        args = self._parse_args(self._argv)
+        data = DataIngest(args['data']).get()
+        with open(args['yaml']) as yaml_file:
+            yaml_dict = yaml.safe_load(yaml_file)
+        PipelineWrapper(yaml_dict).fit_transform()
+
+if __name__ == '__main__':
+    MainWrapper(sys.argv).run()
